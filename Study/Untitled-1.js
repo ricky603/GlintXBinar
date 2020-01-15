179ad925b9aa7656e030141c47a8d77f8ee8e32c
ricky546 <ricky.12genius@gmail.com>
2020-01-15 13:08:55 +0700

    [Ricky] Second Commit

---
+++ Study/Untitled-1.js
@@ -0,0 +1,16 @@
+function calculateSquareArea(p, l) {
+    console.log(p * l)
+};
+
+function calculateCubeVolume(p, l, t) {
+    console.log(p * l * t)
+}
+
+var tinggi, lebar, tinggi;
+tinggi = 12;
+lebar = 12;
+panjang = 12;
+
+calculateCubeVolume(panjang, lebar, tinggi);
+calculateSquareArea(panjang, lebar);
+
